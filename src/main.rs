39d847ae860bb388b5c55935e7bd4406f3ec610a
Kunal Desai <kdesai@figma.com>
2023-10-28 08:16:08 -0700

    be able to read file

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,15 @@
 use std::env;
+use std::fs;
 
 fn main() {
     let args: Vec<String>  = env::args().collect();
 
-    // first arg is assumed to be filename
+    // first arg is assumed to be file path 
     if args.len() <= 1 {
         panic!("Did not specify a file arg")
     }
-    let filename = &args[1];
+    let file_path = &args[1];
 
-
+    let source = fs::read_to_string(file_path).expect("error trying to read source file");
+        println!("text:\n{source}")
 }
