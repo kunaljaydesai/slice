4af34baf6542001e73961b4522c09d962f4de4ff
Kunal Desai <kdesai@figma.com>
2023-10-29 06:44:53 -0700

    support LHS identifier expressions

--- src/parser.rs
+++ src/parser.rs
@@ -14,6 +14,13 @@ impl TokenStream {
         TokenStream { tokens }
     }
 
+    fn peek_next_token(&self) -> Option<&Token> {
+        if self.tokens.len() > 1 {
+            return Some(&self.tokens[1]);
+        }
+        None
+    }
+
     fn expected_current_token(&mut self, expected_token_type: TokenType) {
         if let Some(curr_token) = self.get_current_token() {
             // TODO: implement fmt::Display for TokenType
@@ -108,19 +115,118 @@ impl Visitable for FunctionDeclaration {
     }
 }
 
+pub enum Expression {
+    Binary(BinaryExpression),
+    NumberLiteral(NumberLiteralExpression),
+    Identifier(IdentifierExpression),
+    FunctionCall(FunctionCallExpression),
+}
+
+pub struct BinaryExpression {}
+impl Visitable for BinaryExpression {
+    fn visit(tokens: &mut TokenStream) -> Self {
+        todo!("Implement binary expression");
+    }
+}
+
+pub struct NumberLiteralExpression {}
+impl Visitable for NumberLiteralExpression {
+    fn visit(tokens: &mut TokenStream) -> Self {
+        todo!("Implement number literal expression");
+    }
+}
+
+pub struct IdentifierExpression {
+    name: String,
+}
+impl Visitable for IdentifierExpression {
+    fn visit(tokens: &mut TokenStream) -> Self {
+        tokens.expected_current_token(TokenType::TokIdentifier);
+        if let Some(identifier_token) = tokens.consume() {
+            IdentifierExpression {
+                name: identifier_token.identifier,
+            }
+        } else {
+            panic!("Expected token in identifier expression but didnt find one");
+        }
+    }
+}
+
+pub struct FunctionCallExpression {}
+impl Visitable for FunctionCallExpression {
+    fn visit(tokens: &mut TokenStream) -> Self {
+        todo!("Implement number literal expression");
+    }
+}
+
+impl Expression {
+    fn handleLHS(tokens: &mut TokenStream) -> Expression {
+        if let Some(token) = tokens.get_current_token() {
+            match token.token_type {
+                TokenType::TokIdentifier => {
+                    if let Some(identifier_token) = tokens.get_current_token() {
+                        if let Some(next_token) = tokens.peek_next_token() {
+                            // token after means it can be a variable or a function call
+                            let is_fn_call = next_token.token_type == TokenType::TokLpar;
+                            if is_fn_call {
+                                return Expression::FunctionCall(FunctionCallExpression::visit(
+                                    tokens,
+                                ));
+                            } else {
+                                return Expression::Identifier(IdentifierExpression::visit(tokens));
+                            }
+                        } else {
+                            // no token after the identifier means its just an identifier
+                            // expression
+                            return Expression::Identifier(IdentifierExpression::visit(tokens));
+                        }
+                    } else {
+                        panic!("Expected identifier token but didn't find one");
+                    }
+                }
+                _ => panic!("Received unexpected token type in expression"),
+            }
+        } else {
+            panic!("Expected token in expression but didn't find one");
+        }
+    }
+
+    fn handleBinOpRHS(tokens: &mut TokenStream, lhs: Expression) -> Expression {
+        todo!("handleBinOpRHS");
+    }
+}
+
+impl Visitable for Expression {
+    fn visit(tokens: &mut TokenStream) -> Expression {
+        let lhs = Expression::handleLHS(tokens);
+        Expression::handleBinOpRHS(tokens, lhs)
+    }
+}
+
 enum ScopeStatement {
     Definition(DefinitionScopeStatement),
     Conditional(ConditionalScopeStatement),
     Return(ReturnScopeStatement),
 }
-pub struct DefinitionScopeStatement {}
+pub struct DefinitionScopeStatement {
+    lvalue_identifier: String,
+    rvalue_expression: Expression,
+}
 pub struct ReturnScopeStatement {}
 pub struct ConditionalScopeStatement {}
 
 impl Visitable for DefinitionScopeStatement {
     fn visit(tokens: &mut TokenStream) -> DefinitionScopeStatement {
-        todo!("Need to implement definition statement");
-        DefinitionScopeStatement {}
+        if let Some(lvalue_token) = tokens.consume() {
+            tokens.expected_current_token(TokenType::TokEquals);
+            tokens.consume();
+            DefinitionScopeStatement {
+                lvalue_identifier: lvalue_token.identifier,
+                rvalue_expression: Expression::visit(tokens),
+            }
+        } else {
+            panic!("Expected a token in definition but didn't find one");
+        }
     }
 }
 
@@ -162,6 +268,7 @@ impl Visitable for Scope {
             }
         }
         tokens.expected_current_token(TokenType::TokRbrak);
+        tokens.consume();
         Scope {}
     }
 }
